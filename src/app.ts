09433c8ae6b4b1dedf1219624fb49250db154e36
Ivo106 <ivo.tonioni@gmail.com>
2025-04-20 13:58:33 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,79 @@
+import { join } from 'path'
+import { createBot, createProvider, createFlow, addKeyword, utils } from '@builderbot/bot'
+import { MemoryDB as Database } from '@builderbot/bot'
+import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
+
+const PORT = process.env.PORT ?? 3008
+
+
+const welcomeFlow = addKeyword<Provider, Database>(['Agent'])
+    .addAnswer(`🙌 Hello welcome to this *Chatbot Agent*`)
+    .addAnswer(
+        [
+            'I am a chatbot agent that can help you with your questions and concerns. How can I help you today?',
+        ],
+        { delay: 800, capture: true },
+        async (ctx, { fallBack }) => {
+            if (!ctx.body.toLocaleLowerCase().includes('doc')) {
+                return fallBack('You should type *doc*')
+            }
+            return
+        },
+       
+    )
+
+const main = async () => {
+    const adapterFlow = createFlow([welcomeFlow ])
+    
+    const adapterProvider = createProvider(Provider)
+    const adapterDB = new Database()
+
+    const { handleCtx, httpServer } = await createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    adapterProvider.server.post(
+        '/v1/messages',
+        handleCtx(async (bot, req, res) => {
+            const { number, message, urlMedia } = req.body
+            await bot.sendMessage(number, message, { media: urlMedia ?? null })
+            return res.end('sended')
+        })
+    )
+
+    adapterProvider.server.post(
+        '/v1/register',
+        handleCtx(async (bot, req, res) => {
+            const { number, name } = req.body
+            await bot.dispatch('REGISTER_FLOW', { from: number, name })
+            return res.end('trigger')
+        })
+    )
+
+    adapterProvider.server.post(
+        '/v1/samples',
+        handleCtx(async (bot, req, res) => {
+            const { number, name } = req.body
+            await bot.dispatch('SAMPLES', { from: number, name })
+            return res.end('trigger')
+        })
+    )
+
+    adapterProvider.server.post(
+        '/v1/blacklist',
+        handleCtx(async (bot, req, res) => {
+            const { number, intent } = req.body
+            if (intent === 'remove') bot.blacklist.remove(number)
+            if (intent === 'add') bot.blacklist.add(number)
+
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            return res.end(JSON.stringify({ status: 'ok', number, intent }))
+        })
+    )
+
+    httpServer(+PORT)
+}
+
+main()
